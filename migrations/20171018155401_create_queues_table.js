48f20bc00680b836ddf6824eda0aa3207672a823
Radoslaw Mejer <radmen@desmart.com>
2017-10-18 16:04:35 +0200

    chore(database): add knex migration files

---
+++ migrations/20171018155401_create_queues_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('jobs', table => {
+    table.bigIncrements('id')
+    table.string('queue')
+    table.text('payload', 'longtext')
+    table.integer('attempts').unsigned()
+    table.integer('reserved_at').unsigned().nullable()
+    table.integer('created_at').unsigned()
+    table.index(['queue', 'reserved_at'])
+  })
+}
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('jobs')
+}
